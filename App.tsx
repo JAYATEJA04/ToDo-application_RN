4d7f6d220a26d4289cbebf2044b594df2c12bfd5
Jaya <akulajayateja4444@gmail.com>
2024-01-30 14:40:26 +0530

    Revert "version1, draggable boxes"
    This reverts commit 71dad754c8f0381250cf70b3bd3a6def7565c3fb.

--- App.tsx
+++ App.tsx
@@ -1,13 +1,6 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, {useState} from 'react';
-import {
-  Button,
-  Text,
-  View,
-  FlatList,
-  SafeAreaView,
-  TouchableOpacity,
-} from 'react-native';
+import {Button, Text, View, FlatList, SafeAreaView} from 'react-native';
 import MainUI from './src/components/MainUI';
 import Animate from './src/components/Animation';
 
@@ -17,31 +10,18 @@ const App = () => {
   const handlePress = () => {
     setAnimateBox(prev => [...prev, animateBox]);
   };
-  const count = 0;
 
   return (
-    <View style={{flex: 1}}>
-      <TouchableOpacity
-        style={{
-          height: 50,
-          width: 80,
-          borderWidth: 1,
-          borderRadius: 20,
-          alignItems: 'center',
-          justifyContent: 'center',
-          backgroundColor: 'lightblue',
-        }}
-        onPress={handlePress}>
-        <Text style={{color: 'black'}}>Click me</Text>
-      </TouchableOpacity>
+    <SafeAreaView style={{flex: 1}}>
+      <Button title="Click here" onPress={handlePress}/>
       <FlatList
         data={animateBox}
-        renderItem={() => <Animate boxId={count} />}
+        renderItem={() => <Animate />}
         key={'_'}
         keyExtractor={index => '_' + index.toString()}
         numColumns={2}
       />
-    </View>
+    </SafeAreaView>
   );
 };
 
