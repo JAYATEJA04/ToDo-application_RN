71dad754c8f0381250cf70b3bd3a6def7565c3fb
Jaya <akulajayateja4444@gmail.com>
2024-01-30 14:10:33 +0530

    version1, draggable boxes

--- App.tsx
+++ App.tsx
@@ -1,6 +1,13 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, {useState} from 'react';
-import {Button, Text, View, FlatList, SafeAreaView} from 'react-native';
+import {
+  Button,
+  Text,
+  View,
+  FlatList,
+  SafeAreaView,
+  TouchableOpacity,
+} from 'react-native';
 import MainUI from './src/components/MainUI';
 import Animate from './src/components/Animation';
 
@@ -10,18 +17,31 @@ const App = () => {
   const handlePress = () => {
     setAnimateBox(prev => [...prev, animateBox]);
   };
+  const count = 0;
 
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <Button title="Click here" onPress={handlePress}/>
+    <View style={{flex: 1}}>
+      <TouchableOpacity
+        style={{
+          height: 50,
+          width: 80,
+          borderWidth: 1,
+          borderRadius: 20,
+          alignItems: 'center',
+          justifyContent: 'center',
+          backgroundColor: 'lightblue',
+        }}
+        onPress={handlePress}>
+        <Text style={{color: 'black'}}>Click me</Text>
+      </TouchableOpacity>
       <FlatList
         data={animateBox}
-        renderItem={() => <Animate />}
+        renderItem={() => <Animate boxId={count} />}
         key={'_'}
         keyExtractor={index => '_' + index.toString()}
         numColumns={2}
       />
-    </SafeAreaView>
+    </View>
   );
 };
 
